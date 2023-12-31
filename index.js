dfcf500b3450832e2531dfb0a67b38fb5abc93dd
sandesh079 <sandesh.dahal022@gmail.com>
2023-12-31 10:50:55 +0545

    added code in gitignore

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const users =[
+    {id:323, name:'ram', address:'ktm'},
+    {id:511, name:'kaylin', address:'gorkha'},
+    {id:56, name:'bishwo', address:'gorkha'},
+    {id:456, name:'gopal', address:'ktm'}
+]
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/users', (req, res) => {
+   const filteredUser =  users.filter((item,id)=>{
+      if(item.name[0] === req.query.startswith  && item.name[item.name.length -1] ===req.query.endswith){
+        return item
+      }
+    })
+    res.send(filteredUser[0].name)
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
